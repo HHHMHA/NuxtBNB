e20f4439e58510c0dd8e6b20a45ae2b929b15d12
J2mF <mp4-12cs5@hotmail.com>
2022-07-10 12:08:06 +0300

    Add dummy homes info and component

---
+++ components/HomeCard.vue
@@ -0,0 +1,25 @@
+<template>
+  <img :src="home.images[0]"><br/>
+  {{ home.title }}<br/>
+  {{ home.location.address }} {{ home.location.city }} {{ home.location.state }}<br/>
+  {{ home.guests }} guests<br/>
+  ${{ home.pricePerNight }} / night<br/>
+</template>
+
+<script>
+export default {
+  name: "HomeCard",
+  props: {
+    home: {
+      type: Object,
+      required: true,
+    }
+  },
+}
+</script>
+
+<style scoped>
+img {
+  width: 200px;
+}
+</style>
--- pages/index.vue
+++ pages/index.vue
@@ -1,9 +1,20 @@
 <template>
-  Hello
+  <div>
+    <div v-for="home in homes" :key="home.objectID" style="float: left; margin: 10px">
+      <home-card :home="home"></home-card>
+    </div>
+  </div>
 </template>
 
 <script>
+import homes from "~/data/homes";
+
 export default {
-  name: 'IndexPage'
+  name: "IndexPage",
+  data() {
+    return {
+      homes: homes.slice(0, 3),
+    }
+  },
 }
 </script>
