b61029662be452a4f60282b92e9b93e2ba9d1b12
J2mF <mp4-12cs5@hotmail.com>
2022-07-10 12:17:35 +0300

    Setup dependinces and lint project
    Change Log:
    - fix HomeCard component contains more than one root element

--- components/HomeCard.vue
+++ components/HomeCard.vue
@@ -1,19 +1,22 @@
 <template>
-  <img :src="home.images[0]"><br/>
-  {{ home.title }}<br/>
-  {{ home.location.address }} {{ home.location.city }} {{ home.location.state }}<br/>
-  {{ home.guests }} guests<br/>
-  ${{ home.pricePerNight }} / night<br/>
+  <div>
+    <img :src="home.images[0]" /><br />
+    {{ home.title }}<br />
+    {{ home.location.address }} {{ home.location.city }} {{ home.location.state
+    }}<br />
+    {{ home.guests }} guests<br />
+    ${{ home.pricePerNight }} / night<br />
+  </div>
 </template>
 
 <script>
 export default {
-  name: "HomeCard",
+  name: 'HomeCard',
   props: {
     home: {
       type: Object,
       required: true,
-    }
+    },
   },
 }
 </script>
--- pages/index.vue
+++ pages/index.vue
@@ -1,16 +1,20 @@
 <template>
   <div>
-    <div v-for="home in homes" :key="home.objectID" style="float: left; margin: 10px">
+    <div
+      v-for="home in homes"
+      :key="home.objectID"
+      style="float: left; margin: 10px"
+    >
       <home-card :home="home"></home-card>
     </div>
   </div>
 </template>
 
 <script>
-import homes from "~/data/homes";
+import homes from '~/data/homes'
 
 export default {
-  name: "IndexPage",
+  name: 'IndexPage',
   data() {
     return {
       homes: homes.slice(0, 3),
