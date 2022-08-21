a5142e92c9d99b4d0babef7e78086dc025f789e9
J2mF <mp4-12cs5@hotmail.com>
2022-08-21 18:38:47 +0300

    Add error page

---
+++ layouts/error.vue
@@ -0,0 +1,27 @@
+<template>
+<div style="text-align: center">
+  <h1>Error!</h1>
+  {{ error.message }} {{ error.statusCode }}
+</div>
+</template>
+
+<script>
+export default {
+  name: "error",
+  head() {
+    return {
+      title: 'Big Problems';
+    };
+  },
+  props: {
+    error: {
+      type: Object,
+      required: true,
+    }
+  }
+};
+</script>
+
+<style scoped>
+
+</style>
