52188330520e3c2c7bc132a1b489e0ac874f5c10
Al-Hasan Saleh <alhasan.saleh@kuwaitnet.com>
2022-10-06 12:56:40 +0300

    Add css asset

--- nuxt.config.js
+++ nuxt.config.js
@@ -20,7 +20,9 @@ export default {
   },
 
   // Global CSS: https://go.nuxtjs.dev/config-css
-  css: [],
+  css: [
+    '~/assets/sass/app.scss'
+  ],
 
   // Plugins to run before rendering page: https://go.nuxtjs.dev/config-plugins
   plugins: [
@@ -58,5 +60,10 @@ export default {
   },
 
   // Build Configuration: https://go.nuxtjs.dev/config-build
-  build: {}
+  build: {
+    extractCSS: true,
+    loaders: {
+      limit: 0,
+    },
+  }
 };
