1fa2a5a3681d48a30314b3c9be8df4a2f9988da2
J2mF <mp4-12cs5@hotmail.com>
2022-10-09 12:10:34 +0300

    Style header and fix pacakges

--- layouts/default.vue
+++ layouts/default.vue
@@ -1,8 +1,22 @@
 <template>
-  <div>
-    <header style="background-color: #eee;">
-      <nuxt-link to="/">Home</nuxt-link>
-      <input type="search" ref="citySearch" @change="changed">
+  <div class="app">
+    <header class="app-header">
+      <div class="app-logo">
+        <img src="/images/logo.svg" alt="">
+      </div>
+      <div class="app-search">
+        <input type="search" ref="citySearch" @change="changed" placeholder="Enter your address">
+        <input type="text" class="datepicker" placeholder="Check in">
+        <input type="text" class="datepicker" placeholder="Check out">
+        <button>
+          <img src="/images/icons/search.svg" alt="">
+        </button>
+      </div>
+      <div class="app-user-menu">
+        <img src="/images/icons/house.svg" alt="">
+        <div class="name">Host</div>
+        <img src="/images/user.jpg" class="avatar" alt="">
+      </div>
     </header>
     <Nuxt />
   </div>
