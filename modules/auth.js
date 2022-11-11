673ca7591c233e1b2fc38e4d0b01f4af3bc561a8
J2mF <mp4-12cs5@hotmail.com>
2022-11-11 20:42:34 +0300

    Add token to request and protect api url

--- modules/auth.js
+++ modules/auth.js
@@ -1,4 +1,5 @@
 import cookie from 'cookie'
+import {OAuth2Client} from "google-auth-library";
 
 export default function() {
   const authConfig = this.options.publicRuntimeConfig.auth;
@@ -7,8 +8,38 @@ export default function() {
     app.use('/api', handler);
   })
 
-  function handler(req, res, next) {
+  async function handler(req, res, next) {
     const idToken = cookie.parse(req.headers.cookie)[authConfig.cookieName];
+    if (!idToken) return rejectHit(res);
+
+    const ticket = await getUser(idToken);
+    if (!ticket) return rejectHit(res);
+
+    req.identity = {
+      id: ticket.sub,
+      email: ticket.email,
+      name: ticket.name,
+      image: ticket.picture,
+    }
     next();
+  }
+
+  async function getUser(idToken) {
+    const client = new OAuth2Client(authConfig.clientId);
+    try {
+      const ticket = await client.verifyIdToken({
+        idToken,
+        audience: authConfig.clientId
+      });
+      return ticket.getPayload();
+    }
+    catch (error) {
+      console.log(error);
+    }
+  }
+
+  function rejectHit(res) {
+    res.statusCode = 401;
+    res.end();
   }
 }
