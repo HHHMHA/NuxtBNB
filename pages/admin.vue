b2cfae58ebeea7a354e43c7aeab01a186b507d27
J2mF <mp4-12cs5@hotmail.com>
2022-11-13 21:26:24 +0300

    add basic admin main page

---
+++ pages/admin.vue
@@ -0,0 +1,16 @@
+<template>
+  <div class="p-4">
+    Admin
+    <nuxt-child></nuxt-child>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "admin"
+};
+</script>
+
+<style scoped>
+
+</style>
