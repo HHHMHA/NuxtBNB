e9c4c8185aaa990f581a7f9990c56bafafda6eb5
J2mF <alhasan.saleh@kuwaitnet.com>
2023-03-05 14:57:33 +0300

    feat: use cloudinary for gallery

--- components/PropertyGallery.vue
+++ components/PropertyGallery.vue
@@ -2,9 +2,9 @@
  <div class="app-section">
    <div class="app-wrapper">
      <div class="app-masonry">
-       <div v-for="image in images"
-            :key="image"
-            :style="`background-image: url(${image})`"
+       <div v-for="publicId in images"
+            :key="publicId"
+            :style="`background-image: url(${getImageUrl(publicId)})`"
        >
        </div>
      </div>
@@ -20,6 +20,15 @@ export default {
       type: Array,
       required: true,
     }
+  },
+  methods: {
+    getImageUrl(publicId) {
+      return this.$img(publicId, {
+        width: 600,
+      }, {
+        provider: "cloudinary",
+      });
+    },
   }
 };
 </script>
