b9ad0ac3b564a6c7e394815f824c8f301ba19e7c
J2mF <alhasan.saleh@kuwaitnet.com>
2023-03-19 15:39:30 +0300

    feat: update default layout to use v-calendar

--- layouts/default.vue
+++ layouts/default.vue
@@ -6,9 +6,26 @@
       </div>
       <div class="app-search">
         <input type="search" ref="citySearch" @change="changed" placeholder="Enter your address">
-        <input type="text" class="datepicker" placeholder="Check in">
-        <input type="text" class="datepicker" placeholder="Check out">
-        <button>
+        <client-only>
+          <template #placeholder>
+            <input class="datepicker">
+            <span class="ml-6 mr-2">to</span>
+            <input class="datepicker"><br/>
+          </template>
+
+          <date-picker v-model="range"
+                       is-range
+                       timezone="UTC"
+                       :modelConfig="{timeAdjust: '00:00:00'}"
+          >
+            <template v-slot="{ inputValue, inputEvents }">
+              <input :value="inputValue.start" v-on="inputEvents.start" class="datepicker">
+              <span class="ml-6 mr-2">to</span>
+              <input :value="inputValue.end" v-on="inputEvents.end" class="datepicker"><br/>
+            </template>
+          </date-picker>
+        </client-only>
+        <button @click="search">
           <img src="/images/icons/search.svg" alt="">
         </button>
       </div>
@@ -21,13 +38,26 @@
         <div v-show="!isLoggedIn" class="ml-8" id="googleButton"></div>
       </div>
     </header>
-    <Nuxt />
+    <Nuxt/>
   </div>
 </template>
 
 <script>
 export default {
   name: "default",
+  data() {
+    return {
+      location: {
+        lat: 0,
+        lng: 0,
+        label: '',
+      },
+      range: {
+        start: new Date(),
+        end: new Date(),
+      }
+    };
+  },
   mounted() {
     this.$maps.makeAutoComplete(this.$refs.citySearch);
   },
@@ -40,18 +70,27 @@ export default {
     },
   },
   methods: {
-    changed(event) {
-      const place = event.detail;
-      if (!place.geometry) return;
+    search() {
+      if (!this.location.label) {
+        return;
+      }
 
       this.$router.push({
         name: 'search',
         query: {
-         lat: place.geometry.location.lat(),
-         lng: place.geometry.location.lng(),
-         label: this.$refs.citySearch.value,
+          ...this.location,
+          start: this.range.start.getTime() / 1000,
+          end: this.range.start.getTime() / 1000,
         },
       });
+    },
+    changed(event) {
+      const place = event.detail;
+      if (!place.geometry) return;
+
+      this.location.lat = place.geometry.location.lat();
+      this.location.lng = place.geometry.location.lng();
+      this.location.label = this.$refs.citySearch.value;
     }
   }
 };
